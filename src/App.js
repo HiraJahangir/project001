293af5703aee51a1d0bb56b0a51293b7ad8b1cd5
HiraJahangir <hirajahangir93@gmail.com>
2020-06-15 00:14:36 +0500

    updated by hira

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <br>Updated by HiraJahangir</br>
+          <br/>
+          Updated by HiraJahangir
            
         </p>
         <a
